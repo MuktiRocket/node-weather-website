dc9548c35c69f04181f422359af3cd7ec184e478
jaydeep sarkar <jaydeeps96s@gmail.com>
2021-11-30 15:32:19 +0530

    port set up

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //define paths for express config
 publicDirectoryPath = path.join(__dirname,'../public')
@@ -84,6 +85,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is Started on the port 3000')
+app.listen(port,()=>{
+    console.log('Server is Started on the port ' + port)
 })
